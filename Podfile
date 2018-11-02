9e7912bce3086443c9881363205ec597fc5c94bc
Swagat Mishra <swagat_mishra@bose.com>
2018-11-02 15:26:36 -0400

    new

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SnapTesting' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SnapTesting
+
+  target 'SnapTestingTests' do
+    inherit! :search_paths
+    # Pods for testing
+	pod 'FBSnapshotTestCase'
+  end
+  
+  target 'SnapTestingUITests' do
+      inherit! :search_paths
+      # Pods for testing
+      pod 'FBSnapshotTestCase'
+  end
+  
+
+end
---
+++ SnapTestingTests/SnapTestCase.swift
@@ -0,0 +1,35 @@
+//
+//  SnapTestCase.swift
+//  SnapTestingTests
+//
+//  Created by Swagat Mishra on 10/2/18.
+//  Copyright © 2018 Drowsy. All rights reserved.
+//
+
+import FBSnapshotTestCase
+@testable import SnapTesting
+
+class SnapTestCase: FBSnapshotTestCase {
+        
+    override func setUp() {
+        super.setUp()
+//        recordMode = true
+    }
+    
+    func testViewControllerEmpty() {
+        let vc = ViewController()
+//        FBSnapshotVerifyView(vc.view)
+    }
+    
+    func testViewControllerNormal() {
+        let vc = ViewController()
+        vc.state = .normal(["First", "Second", "Third"])
+//        FBSnapshotVerifyView(vc.view)
+    }
+    
+    func testViewControllerNormal2() {
+        let vc = ViewController()
+        vc.state = .normal(["First", "Second", "Third", "Four", "Five", "six"])
+//        FBSnapshotVerifyView(vc.view)
+    }
+}
